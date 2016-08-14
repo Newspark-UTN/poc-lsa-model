e793e8cc9e7b528fb4b7edcbe8bfb5f926fc290e
tomduhourq <t.duhourq@gmail.com>
2016-08-14 19:16:49 -0300

    Working lemmatizer

--- simple.sbt
+++ simple.sbt
@@ -4,6 +4,13 @@ version:= "0.0.1-SNAPSHOT"
 
 scalaVersion:= "2.11.8"
 
-val cleaningDependencies = Seq("edu.stanford.nlp" % "stanford-corenlp" % "3.6.0")
+val testDependencies = Seq(
+  "org.specs2" % "specs2-core_2.11" % "3.8.4.1-scalaz-7.1"
+)
 
-libraryDependencies ++= cleaningDependencies
+val cleaningDependencies = Seq(
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.6.0",
+  "edu.stanford.nlp" % "stanford-corenlp" % "3.6.0" classifier "models-spanish"
+)
+
+libraryDependencies ++= cleaningDependencies ++ testDependencies
--- src/main/scala/org/edu/utn/newspark/Lemmatize.scala
+++ src/main/scala/org/edu/utn/newspark/Lemmatize.scala
@@ -1,20 +1,60 @@
 package org.edu.utn.newspark
 
+import java.util.Properties
+
 import scala.io.Source
+import edu.stanford.nlp.pipeline._
+import edu.stanford.nlp.ling.CoreAnnotations._
+
+import scala.collection.JavaConverters._
+import scala.collection.mutable
 
 /**
   * PoC object that cleans and tokenizes incoming news using Core NLP.
   *
   * @author tom
   */
-object Lemmatize extends App {
-  val news = Source.fromInputStream(getClass.getResourceAsStream("/news.txt")).getLines.toList
-  val stopwords = Source.fromInputStream(getClass.getResourceAsStream("/stopwords.txt")).getLines.toSet
+final case class News(title: String, content: String)
 
-  // TODO (tom): Start lemmatizing and pass on to RDDS in Spark.
-  news.view.grouped(4)
-    .flatMap(_.filterNot(str => str == "###" || str == "##"))
-    .grouped(2)
-    .map(tuple => (tuple(0), tuple(1)))
-    .foreach(println)
+object Lemmatizer extends App {
+
+  val newsFile = Source.fromInputStream(getClass.getResourceAsStream("/news.txt"))
+  val stopWordsFile = Source.fromInputStream(getClass.getResourceAsStream("/stopwords.txt"))
+  val newsFromFile = try newsFile.getLines.map(_.toLowerCase).toList finally newsFile.close()
+  val stopwords = try stopWordsFile.getLines.toSet finally stopWordsFile.close()
+
+  // Take two lines at a time to form (title, content)
+  val news: List[News] =
+    newsFromFile
+      .grouped(2)
+      .map{ case title :: content :: Nil => News(title, content) }
+      .toList
+
+  val LemmaPipeline = {
+    val props = new Properties()
+    props.setProperty("annotators", "tokenize, ssplit, pos, lemma")
+    props.setProperty("tokenize.language", "es")
+    props.setProperty("pos.model", "edu/stanford/nlp/models/pos-tagger/spanish/spanish-distsim.tagger")
+    new StanfordCoreNLP(props)
+  }
+
+  def isOnlyLetters(s: String) = s forall Character.isLetter
+
+  // output all tokens as lowercase after passing through the nlp pipeline
+  def plainTextToLemmas(content: String, stopwords: Set[String]): mutable.Buffer[String] = {
+    val doc = new Annotation(content)
+    LemmaPipeline.annotate(doc)
+
+    val sentences = doc.get(classOf[SentencesAnnotation])
+
+    for {
+      sentence <- sentences.asScala
+      token <- sentence.get(classOf[TokensAnnotation]).asScala
+      lemma = token.get(classOf[LemmaAnnotation])
+      if lemma.length > 2 && !stopwords.contains(lemma) && isOnlyLetters(lemma)
+    } yield lemma.toLowerCase
+  }
+
+  // place lemmas together with their news again
+  news collect { case News(title, content) => News(title, plainTextToLemmas(content, stopwords) mkString " ") } foreach println
 }
