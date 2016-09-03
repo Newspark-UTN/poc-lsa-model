54ffa1cf1822bf720b07a452876e16685478c31e
Alan Szpigiel <alanszp@gmail.com>
2016-09-03 17:22:21 -0300

    Adding final LSA results

--- src/main/scala/org/edu/utn/newspark/lsa/LSA.scala
+++ src/main/scala/org/edu/utn/newspark/lsa/LSA.scala
@@ -1,13 +1,21 @@
 package org.edu.utn.newspark.lsa
 
+import org.apache.spark.mllib.linalg.{Matrix, SingularValueDecomposition}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 import org.edu.utn.newspark.lemmatizer.News
 import org.edu.utn.newspark.provider.MongoNewsProvider
 
+import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable
 
 object LSA extends MongoNewsProvider with App {
+  // We need the number of concepts to make the SVD, else set it to default.
+  val K = if (args.length > 0) args(0).toInt else 100
+  val topConcepts = if (args.length > 1) args(1).toInt else 10
+  val topTerms = if (args.length > 2) args(2).toInt else 5
+  val topDocuments = if (args.length > 3) args(3).toInt else 3
+
   val conf = new SparkConf()
     .setMaster("local[4]")
     .setAppName("LSA Newspark")
@@ -20,23 +28,20 @@ object LSA extends MongoNewsProvider with App {
   val withoutLemmaRDD: RDD[News] = sc.parallelize(allNews)
 
   // Clean all the documents
-  val lemmatizedRDD: RDD[Seq[String]] = withoutLemmaRDD collect { case News(_, content) => plainTextToLemmas(content, stopwords) }
+  val lemmatizedRDD: RDD[(String, Seq[String])] = withoutLemmaRDD collect { case News(title, content) => (title, plainTextToLemmas(content, stopwords)) }
 
-  // Create a new rdd with the wordcounts (term, how many times the term appears in the document) of all documents.
-  val docTermFrequencies: RDD[mutable.HashMap[String, Int]] = lemmatizedRDD map {terms =>
-    terms.foldLeft(new mutable.HashMap[String, Int]()){
-      (actualMap, term) =>
-        actualMap += term -> (actualMap.getOrElse(term, 0) + 1)
-        actualMap
+  val docTermFrequencies = lemmatizedRDD.mapValues(terms => {
+    val termFreqsInDoc = terms.foldLeft(new mutable.HashMap[String, Int]()) {
+      (map, term) => map += term -> (map.getOrElse(term, 0) + 1)
     }
-  }
-
-  println("PRINTING DOCTERM")
-  docTermFrequencies.collect.foreach(println)
+    termFreqsInDoc
+  })
 
   // We will cache this in memory since we are going to use it 2 times:
   // to calculate idfs and term document matrix
   docTermFrequencies.cache()
+
+  val docIds = docTermFrequencies.map(_._1).zipWithUniqueId().map(_.swap).collectAsMap()
 
   // This will be our accumulator for the document frequencies
   val zero = new mutable.HashMap[String, Int]()
@@ -49,10 +54,10 @@ object LSA extends MongoNewsProvider with App {
    * @param tfs the termFrequencies of a single document of the rdd we cached before.
    * @return the partial dfs result in the mapper executing the merge.
    */
-  def mergeDocumentFrequencies(dfs: mutable.HashMap[String, Int], tfs: mutable.HashMap[String, Int]): mutable.HashMap[String, Int] = {
-    tfs.keySet.foreach { term =>
-    dfs += term -> (dfs.getOrElse(term, 0) + 1)
-  }
+  def mergeDocumentFrequencies(dfs: mutable.HashMap[String, Int], tfs: (String, mutable.HashMap[String, Int])): mutable.HashMap[String, Int] = {
+    tfs._2.keySet.foreach { term =>
+      dfs += term -> (dfs.getOrElse(term, 0) + 1)
+    }
     dfs
   }
 
@@ -74,11 +79,14 @@ object LSA extends MongoNewsProvider with App {
   val documentFrequencies: mutable.HashMap[String, Int] = docTermFrequencies.aggregate(zero)(mergeDocumentFrequencies, combinePartialDocumentFrequencies)
 
   // Generate the inverse document frequencies
-  val idfs: mutable.HashMap[Term, IDF] = documentFrequencies map { case (term, docCount) =>
-    (term, math.log(numDocs.toDouble / docCount))
+  val idfs: mutable.HashMap[Term, IDF] = documentFrequencies map {
+    case (term, docCount) => (term, math.log(numDocs.toDouble / docCount))
   }
 
   val termIds: Map[Term, Index] = idfs.keys.zipWithIndex.toMap
+  println("TOOOOOOOOOOOOOOOOOOOOOOOOOooooooooooooooooooooooooooOOOOOOOOOOOOOOOOOOOOOOM")
+  println(numDocs)
+  println(termIds.keys.size)
 
   // Broadcast this map in order to have it available through all the executors, together with idfs.
   val broadcastTermIds = sc.broadcast(termIds).value
@@ -87,13 +95,59 @@ object LSA extends MongoNewsProvider with App {
   // Create the term document matrix
   import org.apache.spark.mllib.linalg.Vectors
 
-  val termDocMatrix = docTermFrequencies map { document =>
-    val documentLength = document.values.sum
+  val termDocMatrix = docTermFrequencies map { case (_, document) =>
+    val documentTotalTerms = document.values.sum
     val document_TF_IDFS: Seq[(Index, TF_IDF)] = document.collect {
-      case (term, count) if broadcastTermIds.contains(term) =>
+      case (term, count) if broadcastTermIds.contains(term) && broadcastIdfs.contains(term) =>
         // tuples of the form (index, idf-tf)
-        (broadcastTermIds(term), broadcastIdfs(term) * document(term) / documentLength)
+        (broadcastTermIds(term), broadcastIdfs(term) * document(term) / documentTotalTerms)
     }.toSeq
     Vectors.sparse(broadcastTermIds.size, document_TF_IDFS)
+  }
+
+  import org.apache.spark.mllib.linalg.distributed.RowMatrix
+
+  termDocMatrix.cache()
+  val mat = new RowMatrix(termDocMatrix)
+  val svd = mat.computeSVD(K, computeU=true)
+
+
+  def topTermsInTopConcepts(svd: SingularValueDecomposition[RowMatrix, Matrix],
+                            numConcepts: Int, numTerms: Int, termIds: Map[Long, String])
+  : Seq[Seq[(String, Double)]] = {
+    val v = svd.V
+    val topTerms = new ArrayBuffer[Seq[(String, Double)]]()
+    val arr = v.toArray
+    for (i <- 0 until numConcepts) {
+      val offs = i * v.numRows
+      val termWeights = arr.slice(offs, offs + v.numRows).zipWithIndex
+      val sorted = termWeights.sortBy(-_._1)
+      topTerms += sorted.take(numTerms).map {
+        case (score, id) => (termIds(id), score)
+      }
+    }
+    topTerms
+  }
+
+  def topDocsInTopConcepts(svd: SingularValueDecomposition[RowMatrix, Matrix],
+                            numConcepts: Int, numDocs: Int, docIds: scala.collection.Map[Long, String])
+  : Seq[Seq[(String, Double)]] = {
+    val u = svd.U
+    val topDocs = new ArrayBuffer[Seq[(String, Double)]]()
+    for (i <- 0 until numConcepts) {
+      val docWeights = u.rows.map(_.toArray(i)).zipWithUniqueId
+      topDocs += docWeights.top(numDocs).map{
+        case (score, id) => (docIds(id), score)
+      }
+    }
+    topDocs
+  }
+
+  val topConceptTerms = topTermsInTopConcepts(svd, topConcepts, topTerms, termIds.map{ case (term, index) => (index.toLong, term)})
+  val topConceptDocs = topDocsInTopConcepts(svd, topConcepts, topDocuments, docIds)
+  for ((terms, docs) <- topConceptTerms.zip(topConceptDocs)) {
+    println("Concept terms: " + terms.map(_._1).mkString(", "))
+    println("Concept docs: " + docs.map(_._1).mkString(", "))
+    println()
   }
 }
